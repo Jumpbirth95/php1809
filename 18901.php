7f96a85d08e5d30a877c2f50be35b313897b89aa
lys <linyuesheng95@gmail.com>
2018-09-12 17:12:26 +0800

    modified:   18901.php

--- 18901.php
+++ 18901.php
@@ -1,10 +1,9 @@
 <?php
-$cache=opendir("/var/www/html/Test");
+$cache=opendir("/var/www/html/php1809");
 $count=0;
 $countarray=array();
 while(($filename=readdir($cache))!==false){
-	if($filename!=".." && $filename!="."){
-
+	if(!preg_match('/^\./',$filename)){
 		$countarray[$count]=$filename;
 		echo "<form method=\"get\" name=\"form".$count."\" action=\"18902.php\" id=\"mform\">";
 		echo  "<a href=\"javascript:document.form".$count.".submit();\"   >".$countarray[$count]."</a>";
