f6ac36a1b05cf337288f3355da55723c9ec4c65a
lys <linyuesheng95@gmail.com>
2018-09-25 15:25:19 +0800

    new file:   0925.php 	new file:   09251.php 	new file:   09252.php 	new file:   09253.php 	modified:   18902.php

---
+++ 09251.php
@@ -0,0 +1,1 @@
+Todays a good day 
--- 18902.php
+++ 18902.php
@@ -6,28 +6,30 @@ $handle=opendir("/var/www/html/php1809");
 $count=0;
 $fnameary=array();
 while(($check=readdir($handle))!==false){
-$fanmeary[$count]=$check;
+	if(!preg_match('/^\./',$check)){
+$fnameary[$count]=$check;
 $count++;
+	}
 }
+closedir($handle);
+$filenum=1;
 if(empty($filename)){
 	$filename=date("md").".php";
-	echo $filename;
-	for(i=0;i<count($fnameary);i++){
-		if($fnameary[$count]==$filename){
-			break;
+	for($i=0;$i<=count($fnameary);$i++){
+		if($fnameary[$i]==$filename){
+			$i=0;
+			$filename=date("md").$filenum.".php";
+			$filenum++;
+//			echo $filename;
+//			echo "<br>";
+//			echo $filenum;
+//			echo "<br>";	
+//			echo $i;
+//			echo "<br>";
+//			echo count($fnameary);
+//			echo "<br>";
 		}	
 		}	
-
-		closedir($handle);
-if(empty($filename)&&$newfile="new"){
-	$filename=date("md").".php";
-	while(($check=readdir($handle))!==false){
-		if($check==$filename){
-			$filename=date("md").$count."1";
-			break;
-		}	
-}
-	echo $filename;
 }
 if(!empty($textcontent)){
 $file=fopen($filename,"w")or die('unable to open file');
@@ -39,7 +41,7 @@ fclose($file);
 <body>
 <div class="div" align=center>
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" name="form1" enctype="multipart/formdata">
-<label for="file">Text</label>
+<label for="file"><?php echo $GLOBALS["filename"]; ?></label>
 <br>
 <textarea rows="30" cols="100" id="text" name="text" >
 <?php
@@ -48,8 +50,6 @@ $contents=file_get_contents($filename);
 if(!empty($contents)){
 $contents=htmlspecialchars($contents);
 echo $contents;
-}else{
-echo "Can't read file";
 }
 ?>
 </textarea>
@@ -57,14 +57,7 @@ echo "Can't read file";
 <br>
 <input type="button" name="back" value="back" onclick="location='18901.php'" />
 <input type="submit" name="save" value="Save"  />
-<input type="button" name="layertest" value="layer" onclick="layertest();">
 </form>
-<script type="text/javascript" src="layer.js">
-</script>
-<script>
-function layertest(){
-layer.msg("12312");};
-</script>
 
 </div>
 </body>
